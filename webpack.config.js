232c99105470c30cec3e992c2a0a4ca8796ea420
Rizzle <734783679@qq.com>
2016-05-08 16:26:13 +0800

    中断

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: path.join(__dirname, 'assets'),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+        exclude: path.join(__dirname, 'components')
+      },
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+    ]
+  }
+}
